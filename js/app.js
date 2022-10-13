3b48a6d9a5a72b3ead47aa742c35eee38203f16b
Firdavs <firdavseminovhpl@gmail.com>
2022-10-13 05:45:47 -0600

    films home work

---
+++ js/app.js
@@ -0,0 +1,19 @@
+let numberOfFilms = +prompt("Qancha film ko`rgansiz?");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+for (let i = 0; i < 1; i++) {
+    let a = prompt("Oxirgi ko`rgan filmlaringizdan biri?"),
+          b = prompt("Uni qancha baholagan bo`lardingiz?");
+    if (a == null && b == null && a == '' && b == '') {
+        i--;
+    } else{
+        personalMovieDB.movies[a] = b;
+    }
+}
+console.log(personalMovieDB);
